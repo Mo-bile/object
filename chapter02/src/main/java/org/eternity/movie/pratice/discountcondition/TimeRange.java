25bc29d47aabe65850ad2673b037d500e4eb4204
모재영 <mo@utransfer.com>
2026-01-29 22:46:51 +0900

    refactor : TimeRange 내 validate 를 추가하여 타당성 강화

--- chapter02/src/main/java/org/eternity/movie/pratice/discountcondition/TimeRange.java
+++ chapter02/src/main/java/org/eternity/movie/pratice/discountcondition/TimeRange.java
@@ -9,8 +9,15 @@ public class TimeRange {
     private LocalTime endTime;
 
     public TimeRange(LocalTime startTime, LocalTime endTime) {
+        validate(startTime, endTime);
         this.startTime = startTime;
         this.endTime = endTime;
+    }
+
+    public void validate(LocalTime startTime, LocalTime endTime) {
+        if (startTime.isAfter(endTime)) {
+            throw new IllegalArgumentException("끝나는 시간이 시작 시간보다 앞에 올 수 없습니다");
+        }
     }
 
     public boolean contains(LocalDateTime whenScreened) {
